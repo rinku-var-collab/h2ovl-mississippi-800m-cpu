afcef63e36510af091748c5a35624598daf053a8
Varun Varghese <talktovarun@gmail.com>
2025-02-16 20:42:50 -0600

    add files

--- main.py
+++ main.py
@@ -48,9 +48,20 @@ if __name__ == "__main__":
     print(f"Text Question: {question_text}\nResponse: {response_text}")
 
     # Test image-based chat
-    image_file_path = "sample.webp"
+    #image_file_path = "sample.webp"
+    img_cfg_map = {
+        'recipt': {'path': './images/recipt.jpeg', 'question': '<image>\nRead the text and provide word by word ocr for the document.'},
+        'salad_dressing': {'path': './images/salad_dressing.jpeg', 'question': '<image>\nRead the text in the image'},
+        'empty_bag': {'path': './images/empty_bag.jpeg', 'question': '<image>\nRead the text in the image'}
+    }
+    image_file_path = './images/salad_dressing.jpeg'
     question_image = """<image>\n
     Read the text in the image
     """
-    response_image, _ = chat_with_image(model, tokenizer, generation_config, image_file_path, question_image)
-    print(f"Image Question: {question_image}\nResponse: {response_image}")
+
+    for k, conf in img_cfg_map.items():
+        print(f'------------- processing: {k} -------------')
+        image_file_path = conf['path']
+        question_image = conf['question']
+        response_image, _ = chat_with_image(model, tokenizer, generation_config, image_file_path, question_image)
+        print(f"Image Question: {question_image}\nResponse: {response_image}")
